ae7a67688572beaea6343c264c7a2ff636e17ce9
cody bellitto <codybell@buffalo.edu>
2015-02-27 12:34:14 -0500

    Started forward propogation

--- basecode/nnScript.py
+++ basecode/nnScript.py
@@ -206,7 +206,7 @@ def nnObjFunction(params, *args):
     w2 = params[(n_hidden * (n_input + 1)):].reshape((n_class, (n_hidden + 1)))
     obj_val = 0
 
-    #should be (50,785)
+    print "should be (50,785)"
     print w1.shape
 
     #should be (10,51)
@@ -218,9 +218,9 @@ def nnObjFunction(params, *args):
     trans_train = training_data.transpose()
     trans_train_labels = training_label.transpose()
 
-    #should be (784,50000)
+    print "should be (784,50000)"
     print trans_train.shape
-    #should be (784,1)
+    print "should be (784,1)"
     print trans_train_labels.shape
 
     # makes the initial bias set
@@ -229,24 +229,35 @@ def nnObjFunction(params, *args):
     #add the bias row to the bottom of the tranposed training data
     trans_train = np.vstack((trans_train,bias_train))
 
-    #shape should be (785,50000)
+    print "shape should be (785,50000)"
     print trans_train.shape
 
     #calculate the training data
     hidden_layer = w1 * trans_train
     hidden_layer = sigmoid(hidden_layer)
 
-    #should be (50,50000)
+    print "should be (50,50000)"
     print hidden_layer.shape
 
     #add bias row to hidden layer
     bias_hidden = np.ones(1,50000)
     hidden_layer = np.vstack((hidden_layer,bias_hidden))
 
+    print "should be (51,50000)"
+    print hidden_layer.shape
+
     output_layer = w2* hidden_layer
+    output_layer = sigmoid(output_layer)
 
+    print "should be (10,50000)"
+    print output_layer.shape
 
+    ##############################
+    # End of Forward Propogation #
+    ##############################
 
+    #now need to change labelling to be 1-of-k notation for the error function
+    
 
 
 
